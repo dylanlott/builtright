26a80861c35457b096cccb8993783add26f12295
Dylan  Lott <lott.dylan@gmail.com>
2017-08-12 18:14:01 -0600

    populate is working

--- server/controllers/part.js
+++ server/controllers/part.js
@@ -1,39 +1,36 @@
 const Part = require('../models/part.js');
 
-exports.createPart = (req, res, next) => {
+exports.createPart = (req, res) => {
   const part = new Part(req.body);
   part.save()
-    .then((data) => res.status(200).json(data))
-    .catch((err) => res.status(500).json(err));
+    .then(data => res.status(200).json(data))
+    .catch(err => res.status(500).json(err));
 };
 
-exports.listParts = (req, res, next) => {
-  return Part.find(req.params)
-    .limit(req.body.limit || 50)
-    .skip(req.body.skip || 0)
-    .then((data) => {
-      console.log('List Parts; ', data);
-      return res.status(200).json(data);
-    });
+exports.listParts = (req, res) => Part.find(req.params)
+  .limit(req.body.limit || 50)
+  .skip(req.body.skip || 0)
+  .populate('_user')
+  .then((data) => {
+    console.log('List Parts; ', data);
+    return res.status(200).json(data);
+  });
+
+exports.getPart = (req, res) => Part.findById(req.params.id)
+  .populate('_user')
+  .then(data => res.status(200).json(data))
+  .catch(err => res.status(500).json(err));
+
+exports.updatePart = (req, res) => {
+  const newPart = req.body;
+  newPart.updated = Date.now();
+  Part.findByIdAndUpdate(req.params.id, newPart, (err, data) => {
+    if (err) res.status(500).json(err);
+    return res.status(200).json(data);
+  });
 };
 
-exports.getPart = (req, res, next) => {
-  return Part.findById(id)
-    .then((data) => {
-      return res.status(200).json(data);
-    })
-    .catch((err) => {
-      return res.status(500).json(err);
-    })
-};
-
-exports.updatePart = (req, res, next) => {
-
-};
-
-exports.deletePart = (req, res, next) => {
-  return Part.findByIdAndRemove(req.params.id)
-    .then((data) => res.status(200).json(data))
-    .catch((err) => res.status(500).send(err));
-};
+exports.deletePart = (req, res) => Part.findByIdAndRemove(req.params.id)
+  .then(data => res.status(200).json(data))
+  .catch(err => res.status(500).send(err));
 
