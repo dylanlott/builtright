eee7019c6cded3916fa3c4bb8637c64d34fea9be
dylanlott <lott.dylan@gmail.com>
2017-09-20 17:53:45 +0200

    separated dev from docker prod env variables

--- server/logger.js
+++ server/logger.js
@@ -1,12 +1,10 @@
 const bunyanMongoDbLogger = require('bunyan-mongodb-logger');
 const pkg = require('./package.json');
 
-const logURL = `mongodb://${process.env.MONGODB_HOST}:${process.env.MONGODB_PORT}/${process.env.MONGODB_DATABASE_NAME}`;
-
 const logger = bunyanMongoDbLogger({
   name: pkg.name,
   streams: ['stdout', 'mongodb'],
-  url: logURL,
+  url: process.env.DATABASE_URI,
   level: process.env.LOG_LEVEL || 'debug'
 });
 
