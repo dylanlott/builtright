7e3eca8b2cd93ac75414aab3a685fa719bf31ee6
dylanlott <lott.dylan@gmail.com>
2017-10-12 17:09:09 -0700

    Update the analytics event endpoint

--- server/router/admin.js
+++ server/router/admin.js
@@ -5,14 +5,30 @@ const AuthenticationController = require('../controllers/authentication');
 
 const ROLE_ADMIN = constants.ROLE_ADMIN;
 const requireAuth = passport.authenticate('jwt', { session: false });
-const requireAdmin = AuthenticationController.roleAuthorization;
+const role = AuthenticationController.roleAuthorization;
 
-router.post('/', requireAuth, requireAdmin(ROLE_ADMIN), (req, res) => {
-  res.send({ content: 'Admin dashboard is working.' });
+router.post('/', requireAuth, role(ROLE_ADMIN), (req, res) => {
+  return res.send({ content: 'Admin dashboard is working.' });
 });
 
-router.get('/stats', requireAuth, requireAdmin(ROLE_ADMIN), (req, res) => {
-  res.send('OK');
+router.get('/stats', requireAuth, role(ROLE_ADMIN), (req, res) => {
+  return res.send('OK');
+});
+
+router.get('/users', requireAuth, role(ROLE_ADMIN), (req, res) => {
+  return res.status(200).send('route not implemented yet');
+});
+
+router.get('/users/:id', requireAuth, role(ROLE_ADMIN), (req, res) => {
+  return res.status(200).send('get user details route not implemented yet');
+});
+
+router.put('/users/:id', requireAuth, role(ROLE_ADMIN), (req, res) => {
+  return res.status(200).send('edit user not implemented yet');
+});
+
+router.delete('/users/:id', requireAuth, role(ROLE_ADMIN), (req, res) => {
+  return res.status(200).send('delete user not implemented yet');
 });
 
 module.exports = router;
--- server/router/analytics.js
+++ server/router/analytics.js
@@ -4,9 +4,15 @@ const logger = require('../logger');
 
 const requireAuth = passport.authenticate('jwt', { session: false });
 
+/*
+ * Create Analytics Event
+ *
+ * @params    req.body.type - can be error, warn, info, or debug
+ * @params    req.body - object that can contain any data that the client sends
+ */
 router.post('/analytics', requireAuth, (req, res) => {
   logger[req.body.type](req.body);
-  res.send('OK');
+  return res.status(201).send('OK');
 });
 
 module.exports = router;
