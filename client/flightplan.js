394f3d50d0f1f40aa32c0b49ec5ca1f6435ee043
dylanlott <lott.dylan@gmail.com>
2017-11-21 17:03:33 -0500

    updates getting ready for prod

--- client/flightplan.js
+++ client/flightplan.js
@@ -3,41 +3,23 @@ var appName = 'builtright';
 var username = 'dylan';
 var startFile = 'bin/www';
 
-plan.target('dev', {
-	host: '165.227.67.146',
-  username: 'dylan',
+plan.target('prod', {
+  host: '104.236.29.255',
+  username: 'root',
   agent: process.env.SSH_AUTH_SOCK,
-	webRoot: '/var/www/dev.builtrightapp.com/client',
+	webRoot: '/var/www/app/client',
   ownerUser: 'root',
   repository: 'https://github.com/dylanlott/builtright-vue.git',
   branchName: 'master',
   maxDeploys: 10
 });
 
-plan.target('prod', {
-  host: '165.227.67.146',
-  username: 'dylan',
-  agent: process.env.SSH_AUTH_SOCK,
-  webRoot: '/var/www/builtrightapp.com/client',
-  repository: 'https://github.com/dylanlott/builtright-vue.git',
-  branchName: 'master'
-});
-
 plan.local('deploy', function(local) {
-  local.log('pushing changes');
-  local.exec('git commit -am "flightplan deploy"');
-	local.exec('git push origin master');
-});
-
-plan.remote('deploy', function(remote) {
-  remote.log('pulling down changes');
-	remote.with('cd /var/www/builtrightapp.com/client', function() {
-		remote.exec('sudo git pull origin master');
-    remote.log('installing updates and changes');
-		remote.exec('sudo npm install');
-    remote.log('building package');
-		remote.exec('sudo npm run build');
-	});
+  local.log('building client application');
+  local.exec('npm run build');
+  local.transfer('./dist', '/var/www/app/client');
+  local.transfer('index.html', '/var/www/app/client');
+  local.log('files transferred');
 });
 
 plan.remote('check', function(remote) {
--- server/flightplan.js
+++ server/flightplan.js
@@ -1,11 +1,9 @@
 const plan = require('flightplan');
 
 const username = 'root';
-const dev = '159.89.132.122'
-const prod = '165.227.67.146';
 
 plan.target('prod', {
-  host: dev,
+  host: '104.236.29.255',
   username,
   agent: process.env.SSH_AUTH_SOCK,
   webRoot: '/var/www/',
@@ -15,31 +13,34 @@ plan.target('prod', {
 });
 
 plan.local('deploy', function (local) {
-  local.log('transferring files');
   const payload = local.exec('git ls-files', { silent: true });
-  local.transfer(payload, '/var/www/builtright/server', {
-    silent: false,
-    user: username
+  local.transfer(payload, '/var/www/app/server', {
+    silent: false
   });
-  local.log('files transferred');
 });
 
 plan.remote('deploy', function (remote) {
-  remote.with('cd /var/www/builtright/server', () => {
+  remote.with('cd /var/www/app/server', () => {
     remote.log('installing dependencies');
-    remote.exec('sudo npm install');
+    remote.exec('npm install');
     remote.log('reticulating splines');
-    remote.exec('sudo pm2 restart index.js --name builtright');
+    remote.exec('pm2 restart index.js --name builtright');
     remote.log('processes restarted, deploy successful');
-    remote.exec('sudo pm2 list');
+    remote.exec('pm2 list');
+    remote.exec('pm2 logs');
   });
 });
 
+plan.local('env', function (local) {
+  local.transfer('./.env', '/var/www/app/server');
+  local.log('environment settings uploaded');
+});
+
 plan.remote('check', function(remote) {
-  remote.exec('sudo pm2 list');
-  remote.exec('sudo pm2 logs builtright');
+  remote.exec('pm2 list');
+  remote.exec('pm2 logs builtright');
 });
 
 plan.remote('monitor', function(remote) {
-  remote.exec('sudo pm2 monit');
+  remote.exec('pm2 monit');
 });
