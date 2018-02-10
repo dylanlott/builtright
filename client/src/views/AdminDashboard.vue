dcee8f3156ef7211b5ce09019e0b6d0b1cc78e01
Dylan Lott <lott.dylan@gmail.com>
2018-02-10 13:19:05 -0700

    Remove socket.io from admin dashboard

--- client/src/views/AdminDashboard.vue
+++ client/src/views/AdminDashboard.vue
@@ -5,26 +5,26 @@
         <v-toolbar-title class="headline">Admin Dashboard</v-toolbar-title>
       </v-toolbar>
       <v-layout ma-1 pa-2 wrap>
-        <v-flex xs12 sm6 md3 ma-2> 
+        <v-flex xs12 sm6 md3 ma-2>
           <v-card>
             <v-card-title><div class="headline">Active Users</div></v-card-title>
             <v-card-text class="active-users">{{ activeUsers }}</v-card-text>
           </v-card>
         </v-flex>
-        <v-flex xs12 sm6 md3 ma-2> 
+        <v-flex xs12 sm6 md3 ma-2>
           <v-card>
             <v-card-title><div class="headline">Pages</div></v-card-title>
             <v-card-text>
-              <v-list> 
+              <v-list>
                 <v-list-tile v-for="(value, key) in pages">
                   <v-list-tile-title v-text="key"></v-list-tile-title>
                   <v-list-tile-content v-text="value"></v-list-tile-content>
                 </v-list-tile>
               </v-list>
             </v-card-text>
-          </v-card> 
+          </v-card>
         </v-flex>
-        <v-flex xs12 sm6 md3 ma-2> 
+        <v-flex xs12 sm6 md3 ma-2>
           <v-card>
             <v-card-title><div class="headline">Referrers</div></v-card-title>
             <v-card-text>
@@ -35,8 +35,8 @@
                   </v-list-tile>
                 </v-list>
             </v-card-text>
-          </v-card> 
-        </v-flex> 
+          </v-card>
+        </v-flex>
       </v-layout>
     </v-card>
   </v-container>
@@ -45,8 +45,8 @@
 <script>
 import { mapState } from 'vuex'
 import { router } from '../router'
-import io from 'socket.io-client'
-const socket = io('localhost:3000')
+// import io from 'socket.io-client'
+// const socket = io('localhost:3000')
 
 export default {
   name: 'admin',
@@ -62,23 +62,23 @@ export default {
     admin: state => state.admin
   }),
   created () {
-    socket.on('updated-stats', (data) => {
-      console.log('### DATA: ', data)
-      this.pages = data.pages 
-      this.referrers = data.referrers
-      this.activeUsers = data.activeUsers
-    })
+    // socket.on('updated-stats', (data) => {
+    //   console.log('### DATA: ', data)
+    //   this.pages = data.pages
+    //   this.referrers = data.referrers
+    //   this.activeUsers = data.activeUsers
+    // })
   },
   beforeCreated () {
-    this.$store.dispatch('checkRole', this.user.user_id, 'admin') 
+    this.$store.dispatch('checkRole', this.user.user_id, 'admin')
   }
 }
 </script>
 
 <style lang="stylus">
 .active-users
-  font-size: 3em 
+  font-size: 3em
 
 .admin
-  margin: 0px 
+  margin: 0px
 </style>
