7346ceaa0f50b6e48ec7e86d24f47b84f6ce1218
Dylan Lott <lott.dylan@gmail.com>
2018-02-21 21:55:09 -0700

    Update docker-compose, get prod working

--- flightplan.js
+++ flightplan.js
@@ -1,11 +1,11 @@
 const plan = require('flightplan');
-const username = 'dylan';
+const username = 'root';
 
 plan.target('prod', {
-  host: '165.227.67.146',
+  host: '138.68.27.220',
   username: username,
   agent: process.env.SSH_AUTH_SOCK,
-  webRoot: '/var/www/builtrightapp.com/',
+  webRoot: '/opt/builtright/',
   ownerUser: username,
   repository: 'https://github.com/dylanlott/builtright.git',
   branchName: 'master'
@@ -14,23 +14,14 @@ plan.target('prod', {
 plan.local('deploy', function (local) {
   local.log('sending main payload');
   const payload = local.exec('git ls-files', { silent: true });
-  local.transfer(payload, '/var/www/builtrightapp.com/');
+  local.transfer(payload, '/opt/builtright/');
   local.log('transferring secrets');
-  local.transfer('./server/.env', '/var/www/builtrightapp.com/');
-  local.transfer('./server/.env.docker', '/var/www/builtrightapp.com/');
-  local.transfer('./client/.env', '/var/www/builtrightapp.com/');
-  local.transfer('./client/.env.docker', '/var/www/builtrightapp.com');
+  local.transfer('./server/.env', '/opt/builtright/server');
 });
 
 plan.remote('deploy', function (remote) {
-  remote.log('installing dependencies');
-  remote.exec('sudo npm install');
-  remote.log('reloading servers');
-  remote.with('cd /var/www/builtrightapp.com/server', function () {
-    remote.exec('sudo pm2 restart index.js');
+  remote.with('cd /opt/builtright', function () {
+    remote.exec('docker-compose up -d server');
+    remote.exec('docker ps');
   });
 });
-
-plan.local('deploy', function (local) {
-
-})
