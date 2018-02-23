9ca8e425e03d9673af98e12bcddc0be6d9cc40ff
Dylan Lott <lott.dylan@gmail.com>
2018-02-23 07:27:45 +0000

    merge

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -74,7 +74,8 @@ if (process.env.NODE_ENV === 'production') {
   module.exports.plugins = (module.exports.plugins || []).concat([
     new webpack.DefinePlugin({
       'process.env': {
-        NODE_ENV: '"production"'
+        NODE_ENV: '"production"',
+        API_URL: '"api.builtrightapp.com"'
       }
     }),
     new Dotenv({
