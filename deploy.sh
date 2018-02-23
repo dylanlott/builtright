b5b9d6194f82df93d7bc8aa2dc4892c36a7d5f75
Dylan Lott <lott.dylan@gmail.com>
2018-02-23 00:44:17 -0700

    use flightplan entirely

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,9 @@ echo "PULLING LATEST CODE"
 git pull http master
 echo "INSTALLING CLIENT DEPENDENCIES"
 cd client && npm install
+echo "BULIDING CLIENT FILES"
+npm run build
+echo "CLIENT FILES SUCCESSFULLY BUNDLED"
 echo "INSTALLING SERVER DEPENDENCIES"
 cd ../server && npm install
 cd ../
